9b87600052384c50b80de83ab45e66a439f8f406
mohamed amr <dev.mohamedamr@gmail.com>
2023-03-01 22:57:17 +0200

    11

---
+++ session10/main.js
@@ -0,0 +1,24 @@
+// alert("hi external js")
+
+// alert("hi");
+
+
+// alert("hi");alert("by")
+
+
+// var 
+// var Number = 
+// var number = 1;
+// number = 2;
+// number = 4;
+// console.log(number)
+
+
+// var age = [];
+// console.log(  typeof age  )
+
+// condition (  if , switch   )
+// array 
+// loop ( for , while , do while )
+var x  = 10;
+X = 22;
---
+++ session11/main.js
@@ -0,0 +1,162 @@
+// var number = 20
+// console.log(number)
+
+
+//  + , - , * , / 
+
+// console.log( 20 + 10 );
+
+
+// console.log( x / y )    
+// var x = 5;
+// var y = 5;
+// 
+// console.log( x == y ) // true
+
+
+
+
+
+
+
+
+// prompt("hi")
+
+
+// var firstname = prompt("whats your name ?");
+
+
+// if(firstname == "ramez") {
+//     console.log("win !!!!")
+// }else {
+//     console.log("lose")
+// }
+
+
+
+
+
+
+
+
+
+// mohamed , ahmed , eslam
+
+
+// var firstname = prompt("whats ur name ?");
+
+// if(firstname == "mohamed"){
+//     console.log("mohamed win")
+// }
+
+// if(firstname == "eslam"){
+//     console.log("eslam win")
+// }
+
+// if(firstname == "ahmed"){
+//     console.log("ahmed win")
+// }
+
+
+
+
+// if (firstname == "mohamed"){
+//     console.log("mohamed win")
+// }else if (firstname == "ahmed") {
+//     console.log("ahmed win")
+// }else if (firstname == "eslam"){
+//     console.log("eslam win")
+// }else{
+//     console.log("close");
+// }
+
+
+// var oil = prompt("oil ?")
+// var l = prompt("l ?")
+
+
+// if(oil == 90){
+//     console.log(20 * l)
+// }else if  (oil == 92){
+//     console.log(30 * l)
+// }else if (oil == 80){
+//     console.log(10 * l)
+// }else{
+//     console.log("error")
+// }
+
+
+// array
+
+
+// var x = 10
+
+// var users = [ "mohamed" , "ahmed" , "Eslam" ]
+// console.log(users)
+
+// var data = [10,"ahmed",true,[1,2,4]]
+
+// console.log(data)
+
+
+// var phoneNumbser = [012321321,01231230]
+// console.log(phoneNumbser.length)
+// var firstname = prompt("enter your name ?")
+// var users = [ "radwa" , "ahmed" , "ramy" ]
+// if( users.includes(firstname) )
+// {
+//     console.log("name is exist")
+// }else{
+//     console.log("wrong name !")
+// }
+
+// while
+
+// var i = 1;
+// while(10>i)
+// {
+//     i = i + 1
+//     console.log("hi amit")
+// }
+
+
+
+// var users  = ["samy" , "ramy" , "tamer","ahmed","said","radwa"]
+
+// // console.log(users[0])
+// var i = 0;
+// while(users.length>i)
+// {
+//     console.log(users[i])
+//     i = i + 1
+// }
+
+
+
+
+
+
+
+// loop 
+// 1
+// 2
+// 3
+// 4
+// your name
+// 6
+// 7
+// 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
