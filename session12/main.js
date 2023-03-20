718c65b2ff1826cc442ce04b5e416231c836a8cf
mohamed amr <dev.mohamedamr@gmail.com>
2023-03-20 20:21:39 +0200

    12

---
+++ session12/main.js
@@ -0,0 +1,163 @@
+// var 
+// conditions
+// loop 
+// array 
+// functions
+
+// var age
+// // (  console , widnow , documnet  )
+// var username = "mohamed"
+// age = 28
+// age = 30
+// // typeof
+// console.log(address)
+
+
+
+// var x = 10
+// var y = 20
+// x =30
+// console.log( x > y )
+
+// if(10<20)
+// {
+//     console.log(" condtion true ")
+// }
+
+
+// var age = prompt("how old are you")
+
+// if(age >= 30)
+// {
+//     console.log("welcom")
+// }else{
+//     console.log("error")
+// }
+
+
+
+// var deg = prompt("deg ?")
+
+// < 50 فاشل 
+// 90
+// deg > 50 && deg < 70  // جيد 
+// deg  > 70 || deg > 90  //  جدا جيد 
+// deg  > 90 || deg > 100  //  امتياز 
+
+
+
+// console.log(  10 < 20 && 30 > 10 )
+//           true || false (   true   )
+//           true && false (   false   )
+
+
+//and  ( و )
+//or  ( او )
+
+
+
+
+
+// var deg = prompt("deg ?")
+
+// if(deg < 50)
+// {
+//     console.log("ساقط")
+// }else if (deg >= 50 && deg <= 60)
+// {
+//     console.log("جيد")
+// }
+// else if (deg >= 61 && deg <= 80)
+// {
+//     console.log(" جيد جدا")
+// }
+// else if (deg >= 81 && deg <= 100)
+// {
+//     console.log(" امتياز")
+// }else{
+//     console.log("جرب تاني ")
+// }
+
+// array
+// var age = 2030
+//        0   1   2
+// var age = [20,30,40,50,60]
+
+// console.log( age  )
+
+
+// loop
+// while
+// do while 
+// for  
+
+// var i = 0
+// while(10>i){
+//     if(i == 5){
+//         console.log("mohamed amr")
+//     }else{
+//         console.log(i)
+//     }
+//     i = i + 1
+// }
+
+// 0 
+// 1
+// 2 
+// 3
+// 4
+// mohamed amr
+// 6
+// 7
+// 8
+
+
+
+
+
+
+
+
+
+
+
+// fizzbuzz
+
+var n = 20
+// 3 => fizz
+// 5 => buzz
+// 3,5 => fizzbuzz
+// %
+var i = 1
+while(i<=n){
+    if (i%5 == 0 && i%3 == 0){
+        console.log("fizzbuzz")
+    }else if (i%5 == 0){
+        console.log("buzz")
+    }
+    else if (i%3 == 0){
+        console.log("fizz")
+    }
+    else{
+        console.log(i)
+    }
+    i = i +1
+}
+
+// two sum
+var number = [1,4,3,2]
+// var target = 6
+
+var i= 0
+while(i<number.length){
+    console.log(number[i])
+    i = i +1
+}
+
+
+
+// document.write("<table border=1>")
+// document.write("<tr>")
+// document.write("<td>www</td>")
+// document.write("</tr>")
+// document.write("</table>")
