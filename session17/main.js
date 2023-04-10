4e47efa83f0637e349c2242cc5c6066f29901e8f
mohamed amr <dev.mohamedamr@gmail.com>
2023-04-10 22:29:31 +0200

    crud

--- session17/main.js
+++ session17/main.js
@@ -1,7 +1,20 @@
 var task = document.querySelector("#task")
 var save = document.querySelector("#save")
+var res = document.querySelector("#res")
 var todo = []
 save.onclick = function(){
     todo.push(task.value)
+    task.value = ""
     console.log(todo)
+    show()
 }
+
+
+function show(){
+    var resText = ""
+    for(var i =0;i<todo.length;i++){
+        resText += "<li>"+todo[i]+"</li>"
+    }
+    res.innerHTML = resText
+}
+
