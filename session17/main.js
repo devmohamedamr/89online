7efb538cea9eeaae18645d9d55028b2fd7d4fb82
mohamed amr <dev.mohamedamr@gmail.com>
2023-04-10 23:50:52 +0200

    crud

--- session17/main.js
+++ session17/main.js
@@ -1,7 +1,9 @@
 var task = document.querySelector("#task")
 var save = document.querySelector("#save")
+var update  = document.querySelector("#update")
 var res = document.querySelector("#res")
 var todo = []
+
 save.onclick = function(){
     todo.push(task.value)
     task.value = ""
@@ -25,8 +27,18 @@ function destroy(index)
     todo.splice(index,1)
     show()
 }
-
+var id
 function edit (index) { 
     task.value = todo[index]
+    update.style.display = "block"
+    save.style.display = "none"
+    id = index
     // alert()
+ }
+
+
+ update.onclick = function(){
+    todo[id] = task.value 
+    console.log(todo)
+    show()
  }
