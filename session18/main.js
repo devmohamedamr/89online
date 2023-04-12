04803c7daa5a6b5d7721f05267a3c4ff175a435a
mohamed amr <dev.mohamedamr@gmail.com>
2023-04-13 00:38:18 +0200

    api

--- session18/main.js
+++ session18/main.js
@@ -7,24 +7,24 @@
 
 // ================== xmlhttprequest ===================
 
-// 1
-var ajax = new XMLHttpRequest
-// 2 
-ajax.open("GET","https://jsonplaceholder.typicode.com/users")
-// 3
-ajax.send()
-// 4
-ajax.onreadystatechange = function(){
+// // 1
+// var ajax = new XMLHttpRequest
+// // 2 
+// ajax.open("GET","https://jsonplaceholder.typicode.com/users")
+// // 3
+// ajax.send()
+// // 4
+// ajax.onreadystatechange = function(){
 
-    if(ajax.readyState == 4){
-        var data = JSON.parse(ajax.response)
-        var res = ""
-        for(var i =0;i<data.length;i++){
-           res += `<li> ${data[i].name} </li>`
-        }
-        document.getElementById("res").innerHTML = res
-    }
-}
+//     if(ajax.readyState == 4){
+//         var data = JSON.parse(ajax.response)
+//         var res = ""
+//         for(var i =0;i<data.length;i++){
+//            res += `<li> ${data[i].name} </li>`
+//         }
+//         document.getElementById("res").innerHTML = res
+//     }
+// }
 
 
 
@@ -35,3 +35,25 @@ ajax.onreadystatechange = function(){
 // ]
 
 // console.log(users[0].name)
+
+
+
+
+
+
+
+document.getElementById("get").onclick = function(){
+    var username = document.getElementById("username").value
+
+
+    var ajax = new XMLHttpRequest
+    ajax.open("GET","https://api.github.com/users/"+username)
+    ajax.send()
+    ajax.onreadystatechange = function(){
+        if(ajax.readyState == 4){
+            var data = JSON.parse(ajax.response)
+            console.log(data.avatar_url)
+            document.getElementById("img").src = data.avatar_url
+        }
+    }
+}
