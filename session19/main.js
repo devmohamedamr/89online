fb58034ff59afb9209bca46bc1038d287e837b6a
mohamed amr <dev.mohamedamr@gmail.com>
2023-05-08 19:55:15 +0300

    20

--- session19/main.js
+++ session19/main.js
@@ -1,20 +1,36 @@
 var task = document.querySelector("#task")
 var save = document.querySelector("#save")
 var res = document.querySelector("#res")
+show()
+var todos  = JSON.parse(localStorage.getItem("todos"))
 
-var todos  = []
 save.onclick = function(){
     var ob = {name:task.value,status:0}
     todos.push(ob)
-    console.log(todos)
+     var jsondata =  JSON.stringify(todos)
+    localStorage.setItem("todos",jsondata)
     show()
 }
 
 
 function show(){
+    var jsontodo = localStorage.getItem("todos")
+    var tasks = JSON.parse(jsontodo)
     var text = ""
-    for(var i=0;i<todos.length;i++) {
-        text += `<li>${todos[i].name}</li>`
+    for(var i=0;i<tasks.length;i++) {
+        var status = (tasks[i].status == 1) ? "line-through" : "none"
+        text += `<li style='text-decoration:${status}' onclick='done(${i})'>${tasks[i].name}</li>`
     }
     res.innerHTML = text
+}
+
+
+function done(index){
+    if(todos[index].status == 0){
+        todos[index].status = 1
+    }else{
+        todos[index].status = 0
+    }
+    console.log(todos)
+    show()
 }
---
+++ session20/main.js
@@ -0,0 +1,192 @@
+// hoisting 
+
+
+// var i = 30
+
+// for(var i = 0;i<10;i++){
+//     console.log(i)
+// }
+
+// console.log(i)
+// console.log(i)
+
+// let
+
+
+// let x = 10
+// console.log(x)
+
+
+
+
+// let i = 30
+
+// for(let i = 0;i<10;i++){
+//     console.log(i)
+// }
+
+// console.log(i)
+
+
+// const
+// let connection = "mysql"
+// connection = "pdo"
+// console.log(connection)
+// const CONNECTION = "mysql"
+
+
+// let person = {fname:"John", lname:"Doe", age:25};
+// for(let key in person){
+//     console.log(person[key])
+// }
+// ======================================================
+// let persons = ['ahmed','mohamed','eslam']
+// for(var p of persons){
+//     console.log(p)
+// }
+
+
+
+
+// let users = ["mpohamed","ahmed","eslam"]
+
+// users.forEach(function(abdo){
+//     console.log(abdo)
+// })
+
+// var name = "mohamed"
+// console.log("hi "+name)
+// console.log(`hi ${name}`)
+
+
+// let user = {
+//     name:"mohamed",
+//     age:22,
+//     email:"a@a.com"
+// }
+
+// let user2 = {
+//     name:"ahmed",
+//     age:20,
+//     email:"a@a.com"
+// }
+
+// let {name:n,age:a} = user
+// let {name,age} = user2
+
+// console.log(n,a)
+
+// function mult (x,y=1){
+//     console.log(x*y)
+// }
+// mult(10,5)
+
+
+
+// var number = [10,20,30,44]
+
+// console.log(...number)
+
+
+// console.log(...number)
+// var numberprivate = [1,2,...number]
+
+// console.log(numberprivate)
+
+
+
+
+// username1 = "mohamed"
+// age1 = 22
+// // ----
+// username2 = "ahmed"
+
+
+// var user2 = {
+//     name:"ahmed",
+//     age:33
+// } 
+
+// var user1 = {
+//     name:"mohamed",
+//     age:22
+// } 
+
+// var user1 = new Object
+// user1.name = "mohamed"
+// user1.age = 22
+
+// console.log(user1)
+
+// var number = [11,20]
+
+// var number = new Array
+// number[0] = 11
+// number[1] = 20
+
+// console.log(number)
+
+
+// var username = "mohamedamr"
+// [] , {}
+// var username = new String
+// username = "mohamed"
+
+// console.log(username)
+
+
+// datatype 
+
+
+// = new Array , Object , String , Integer
+
+
+// var x = 10
+
+// var number  = new Array
+// number[0] = 10
+// number[1] = 20 
+// var number = [10,20]
+
+// // console.log(number)
+
+
+// [] => array 
+// {} => object 
+// "" => string
+
+
+
+
+// function user () {
+//     console.log(this)
+// }
+
+
+
+
+
+// function hamada(){
+//     console.log(this)
+// }
+
+
+// var s = new hamada
+
+
+
+// factory
+
+
+// function user (n,e){
+//     this.name = n
+//     this.email = e
+//     console.log(this)
+// }
+
+
+// let ahmed = new user("ahmed","a@a.com")
+// let mohamed = new user("mohamed","mohamed@a.com")
+
+
+
